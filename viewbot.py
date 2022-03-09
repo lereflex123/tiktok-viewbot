141829c847778260cd34d873fec6d915ab4217ea
Wykt <47749428+Wykt@users.noreply.github.com>
2022-03-09 18:52:57 +0100

    Update viewbot.py

--- viewbot.py
+++ viewbot.py
@@ -41,7 +41,7 @@ def stats(item_id):
     global count
     while True:
         try:
-            with r.post(f"https://api.toutiao50.com/aweme/v1/aweme/stats/?channel=googleplay&device_type=SM-G9250&device_id={random.randint(1000000000000000000, 9999999999999999999)}&os_version=10&version_code=220400&app_name=musically_go&device_platform=android&aid=1340", headers={"content-type": "application/x-www-form-urlencoded; charset=UTF-8", "user-agent": "com.zhiliaoapp.musically.go/220400 (Linux; U; Android 10; en_US; SM-G9250; Build/MMB25K.G9250ZTU5DPC5; Cronet/TTNetVersion:5f9540e5 2021-05-20 QuicVersion:47555d5a 2020-10-15)"}, data=f"item_id={item_id}&play_delta=1", stream=True, verify=False) as response:
+            with r.post(f"https://api.toutiao50.com/aweme/v1/aweme/stats/?channel=googleplay&device_type=SM-G9250&device_id={random.randint(1000000000000000000, 9999999999999999999)}&os_version=10&version_code=220400&app_name=musically_go&device_platform=android&aid=1340", headers={"content-type": "application/x-www-form-urlencoded; charset=UTF-8", "user-agent": "com.zhiliaoapp.musically.go/220400 (Linux; U; Android 10; en_US; SM-G9250; Build/MMB25K.G9250ZTU5DPC5; Cronet/TTNetVersion:5f9540e5 2021-05-20 QuicVersion:47555d5a 2020-10-15)"}, data=f"item_id={item_id}&play_delta=1", stream=True, verify=False, timeout=10) as response:
                 if (response.json()["status_code"] == 0):
                     count += 1
                     views.append(int(time.time()))
@@ -67,5 +67,5 @@ Thread(target=title_daemon, daemon=True).start()
 for _ in iter(int, 1):
     while True:
         if (active_count() <= threads):
-            Thread(target=(stats), args=(item_id)).start()
+            Thread(target=(stats), args=(item_id,)).start()
             break
